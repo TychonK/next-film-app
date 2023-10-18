97bd20fca001bdaf3b6dafe8f7f1fb02752b2d2d
Tychon <tisha.kruglyak@gmail.com>
2023-10-19 01:39:09 +0200

    Update title2.js
    update title for movies tv and people design

--- src/components/title2.js
+++ src/components/title2.js
@@ -1,6 +1,6 @@
 export default function Title2({ text }) {
     return (
-      <h2 className="absolute -top-52 -left-14 pointer-events-none font-extralight text-xxl text-gray-500">
+      <h2 className="absolute -top-20 -left-6 lg:-top-36 lg:-left-14 pointer-events-none font-extralight text-8xl lg:text-xxl text-gray-500">
         {text}
       </h2>
     );
