cdd2ead12cc01bf77d3d0c57fdfdaf03bb088e18
Tychon <tisha.kruglyak@gmail.com>
2023-10-24 09:29:33 +0200

    people details design update
    updated the design of people details page. Added visit gallery link. to be worked on. Read mor eand read less button for biography.

--- src/pages/people/[id].js
+++ src/pages/people/[id].js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import axios from "axios";
 import useSWR from "swr";
 import { useRouter } from "next/router";
@@ -54,7 +55,13 @@ export default function PersonDetailsPage() {
           console.log(er);
         });
       return data;
-    }
+  }
+  
+    const [showFullText, setShowFullText] = useState(false);
+
+    const toggleText = () => {
+      setShowFullText(!showFullText);
+    };
     
     if (isLoading) {
       return <Loader />;
@@ -67,40 +74,63 @@ export default function PersonDetailsPage() {
   return (
     <div className="text-white px-4 md:px-32">
       <GoBackBtn />
-
-      <div
-        className="relative flex items-end justify-start w-full text-left bg-center bg-cover cursor-pointer h-96 md:col-span-2 lg:row-span-2 lg:h-full group dark:bg-gray-500"
-        style={{
-          backgroundImage: `linear-gradient(rgba(236, 72, 184, 0.3), rgba(236, 72, 184, 0.5)), url(${
-            baseUrlBig + data.images.profiles[0].file_path
-          })`,
-        }}
-      >
-        <h2 className="font-medium text-md group-hover:underline lg:text-2xl lg:font-semibold text-gray-100 p-5">
+      <div className="flex flex-row my-12 rounded-md bg-gray-950">
+        <div
+          className="flex flex-col justify-between text-left bg-center bg-cover cursor-pointer group bg-gray-500 p-4 rounded-md"
+          style={{
+            backgroundImage: `linear-gradient(rgba(50, 0, 114, 0.3), rgba(50, 0, 114, 0.5)), url(${
+              baseUrlBig + data.images.profiles[0].file_path
+            })`,
+          }}
+        >
+          <div className="flex justify-between items-center">
+            <p className="font-semibold text-2xl">
+              [ {data.images.profiles.length} ]
+            </p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              viewBox="0 0 100 100"
+              fill="none"
+            >
+              <circle cx="50" cy="50" r="45" fill="transparent" />
+              <path
+                d="M36.4444 22.5951V77.4051L77.4056 50L36.4444 22.5951Z"
+                fill="#FF6347"
+                stroke="#FF6347"
+                stroke-width="5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
+          <h2 className="flex justify-self-end font-medium text-md group-hover:underline lg:text-2xl lg:font-semibold text-gray-100">
             Visit the gallery
-        </h2>
+          </h2>
+        </div>
+        <div className="py-8 px-12">
+          <h1 className="text-4xl font-extralight">{data.name}</h1>
+          <p className="text-2xl mt-7">Person ID: {id}</p>
+          <p className="text-2xl">
+            Age: {calculateAge(data.birthday)}{" "}
+            <span className="italic">({data.birthday})</span>
+          </p>
+          <p className="text-2xl">Place of birth: {data.place_of_birth}</p>
+          <p className="text-2xl">Known for: {data.known_for_department}</p>
+        </div>
       </div>
-
-      <h1 className="text-4xl font-extralight">{data.name}</h1>
-      <p className="text-2xl">Person ID: {id}</p>
-      <p className="text-2xl">
-        Age: {calculateAge(data.birthday)}{" "}
-        <span className="italic">({data.birthday})</span>
-      </p>
-      <p className="text-2xl">Place of birth: {data.place_of_birth}</p>
       <div>
-        <p className="text-xl font-semibold">Bio:</p>
-        <p>{data.biography}</p>
-      </div>
-      <div className="scroll-container flex flex-row overflow-x-scroll space-x-4 my-8">
-        {data.images.profiles.map((profile) => (
-          <img
-            src={baseUrlBig + profile.file_path}
-            alt="person img"
-            className="w-64"
-            key={profile.file_path}
-          />
-        ))}
+        <p className="text-6xl font-semibold">Biography:</p>
+        <p className={`line-clamp-${showFullText ? "none" : "6"} mt-4`}>
+          {data.biography}
+        </p>
+        <button
+          onClick={toggleText}
+          className="text-violet-400 hover:underline focus:outline-none ml-auto block"
+        >
+          {showFullText ? "Read Less" : "Read More"}
+        </button>
       </div>
       <SimilarMovies
         data={data.combined_credits.cast}
