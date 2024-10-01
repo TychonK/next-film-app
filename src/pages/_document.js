5935aaaa07080025f3647733f1f83ad4bc9f138c
Tychon <tisha.kruglyak@gmail.com>
2024-10-01 16:18:22 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,13 @@ export default function Document() {
 
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Next film App | Search films, series and actors online</title>
+        <meta>
+          Next.js film application. Searching all the nesessary information
+          about any film, series and actor.
+        </meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
