e402df96e4d122958eebfcd0709302e905ec2a05
Tychon <78542451+TychonK@users.noreply.github.com>
2025-02-25 19:52:38 +0100

    Update CardPpl.js

--- src/components/CardPpl.js
+++ src/components/CardPpl.js
@@ -3,14 +3,9 @@ import Link from "next/link";
 export default function CardPpl(props) {
   const { personData } = props;
   const baseUrlImg = "https://image.tmdb.org/t/p/w500/";
-
   return (
-    <Link
-      href={`/people/${personData.id}`}
-    >
-      <div
-        className="w-card/sm md:w-card mx-auto rounded-lg md:p-4 hover:scale-105 duration-300 group"
-      >
+    <Link href={`/people/${personData.id}`}>
+      <div className="w-card/sm md:w-card mx-auto rounded-lg md:p-4 hover:scale-105 duration-300 group">
         <img
           className="w-20 h-20 md:w-36 md:h-36 rounded-full mx-auto object-cover object-center"
           src={baseUrlImg + personData.profile_path}
@@ -20,7 +15,10 @@ export default function CardPpl(props) {
           {personData.name}
         </h2>
         <p className="text-center text-gray-600 mt-1">
-          {personData.known_for_department.replace(/ing/g, "or")}
+          {personData.known_for_department ? personData.known_for_department.replace(
+            /ing/g,
+            "or"
+          ) : ""}
         </p>
         <div className="flex justify-center mt-2 md:mt-5">
           <p className="text-blue-500 hover:text-blue-700 mx-3 group-hover:underline">
